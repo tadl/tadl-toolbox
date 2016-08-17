bfab280cf7c59c3530cbf5b78531fc34e658cf2b
Scott Morey <swmorey@gmail.com>
2016-08-17 15:08:30 -0400

    memcached for cache in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,14 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Use a different cache store in production.
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHED_URL"] || ""),
+                    {:failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
