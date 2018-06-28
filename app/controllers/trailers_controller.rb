f130f18fb044ae84b6bf74c880eb6e1603f06b96
Scott Morey <swmorey@gmail.com>
2018-06-28 16:32:10 -0400

    fix so dokku will let me deploy

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -1,8 +1,19 @@
 class TrailersController < ApplicationController
 	before_action :authenticate_user!, :except => [:get_trailer]
-	before_filter(:only => :) do |controller|
+	before_filter(:only => :get_trailer	) do |controller|
    		authenticate_user! unless controller.request.format.json?
  	end
   	before_filter :set_headers
 	respond_to :html, :json, :js
+
+	def queue
+	end
+
+	def search_by_title
+	end
+
+	def add_record
+	end
+
+
 end
