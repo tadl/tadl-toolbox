e143d7411d0842bbbc62c237d92ee3d273feb700
Scott Morey <swmorey@gmail.com>
2019-03-07 12:09:25 -0500

    gem updates

---
+++ db/migrate/20180628205048_create_trailers.rb
@@ -0,0 +1,6 @@
+class CreateTrailers < ActiveRecord::Migration
+  def change
+    create_table :trailers do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822194907) do
+ActiveRecord::Schema.define(version: 20180628205048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 20160822194907) do
     t.integer  "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "trailers", force: :cascade do |t|
   end
 
 end
